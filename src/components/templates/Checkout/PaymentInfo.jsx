86a0d142805b6f6b290b010db619e258961282a3
MBing <martin.demiddel@pm.me>
2023-02-02 18:05:37 +0100

    feat(template): add checkout templates

---
+++ src/components/templates/Checkout/PaymentInfo.jsx
@@ -0,0 +1,3 @@
+const PaymentInfo = () => {};
+
+export { PaymentInfo };
---
+++ src/components/templates/Checkout/index.js
@@ -0,0 +1,5 @@
+import { Checkout } from "./Checkout";
+import { Confirm } from "./Confirm";
+import { PaymentInfo } from "./PaymentInfo";
+
+export { Checkout, Confirm, PaymentInfo };
